093c232282f9893baf602b488568e936f5d6ae34
danimisme <msubhanr53@gmail.com>
2024-04-01 21:51:10 +0700

    create loginPage

---
+++ src/app/(auth)/login/page.js
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/Fragments/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="row">
+      <div className="col-md-3 offset-md-3 my-5">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
