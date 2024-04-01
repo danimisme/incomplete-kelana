b973f27035a99347bd9b0654bfe721360376bbd0
danimisme <msubhanr53@gmail.com>
2024-04-01 23:08:51 +0700

    create register page

---
+++ src/app/(auth)/register/page.js
@@ -0,0 +1,11 @@
+import RegisterForm from "@/components/Fragments/RegisterForm";
+
+export default function RegisterPage() {
+  return (
+    <div className="row d-flex vh-100 align-items-center justify-content-center">
+      <div className="col-md-3">
+        <RegisterForm />
+      </div>
+    </div>
+  );
+}
