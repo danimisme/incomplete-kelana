df86e69aef08b5f190b989b5f32527bcd2508536
danimisme <msubhanr53@gmail.com>
2024-04-01 23:07:32 +0700

    modify login page styles

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -2,8 +2,8 @@ import LoginForm from "@/components/Fragments/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="row">
-      <div className="col-md-3 offset-md-3 my-5">
+    <div className="row d-flex vh-100 align-items-center justify-content-center">
+      <div className="col-md-3">
         <LoginForm />
       </div>
     </div>
