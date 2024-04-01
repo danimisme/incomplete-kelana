718e6f167a5f9943f2fbd6ec81efc3c950edbb4e
danimisme <msubhanr53@gmail.com>
2024-04-02 00:57:54 +0700

    modify responsive media query layout

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -3,7 +3,7 @@ import LoginForm from "@/components/Fragments/LoginForm";
 export default function LoginPage() {
   return (
     <div className="row d-flex vh-100 align-items-center justify-content-center">
-      <div className="col-md-3">
+      <div className="col-lg-3 col-md-6 col-10 my-5">
         <LoginForm />
       </div>
     </div>
