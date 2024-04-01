d24e65656cb493df1570ffe9bf6de84229bf1a8c
danimisme <msubhanr53@gmail.com>
2024-04-02 00:22:59 +0700

    modify style of registerPage

--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -3,7 +3,7 @@ import RegisterForm from "@/components/Fragments/RegisterForm";
 export default function RegisterPage() {
   return (
     <div className="row d-flex vh-100 align-items-center justify-content-center">
-      <div className="col-md-3">
+      <div className="col-lg-6 col-md-8 offset-lg-3 col-10">
         <RegisterForm />
       </div>
     </div>
