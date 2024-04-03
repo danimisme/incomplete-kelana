5487a3f1adda4447387322b15e36fb947fecf2f1
danimisme <msubhanr53@gmail.com>
2024-04-03 23:04:20 +0700

    create jumbotron in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,21 @@
+import { Merriweather } from "next/font/google";
+import styles from "./page.module.css";
+
+const merriweather = Merriweather({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+});
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-danger">Kelana</h1>
+    <div
+      className={` ${styles.jumbotron} d-flex align-items-center justify-content-center text-white`}
+    >
+      <h1
+        className={`${merriweather.className} ${styles.text_shadow} text-center`}
+      >
+        Berkelana Menjelajahi <br /> Semesta Yang Indah
+      </h1>
     </div>
   );
 }
