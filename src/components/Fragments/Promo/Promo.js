4bddd85d61371c7096b5741eabd93976c6d6be02
danimisme <msubhanr53@gmail.com>
2024-04-04 22:43:31 +0700

    get data promos using useGetData

--- src/components/Fragments/Promo/Promo.js
+++ src/components/Fragments/Promo/Promo.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+import styles from "./Promo.module.css";
+import useGetData from "@/services/useGetData";
+
 export default function Promo() {
+  const [promos, setPromos] = useState([]);
+  const { getData } = useGetData();
+
+  useEffect(() => {
+    getData("promos").then((res) => setPromos(res.data.data));
+  }, []);
+  console.log(promos);
   return (
     <div>
       <h1>Promo</h1>
