83c04de7d45a0f59caef5c84f7b82986b43fe304
danimisme <msubhanr53@gmail.com>
2024-04-06 21:18:18 +0700

    add CardPromo in PromoPage

--- src/app/promo/page.js
+++ src/app/promo/page.js
@@ -1,4 +1,5 @@
 "use client";
+import CardPromo from "@/components/Fragments/CardPromo/CardPromo";
 import useGetData from "@/services/useGetData";
 import { useEffect, useState } from "react";
 
@@ -13,18 +14,7 @@ export default function PromoPage() {
       <h1>Promo Page</h1>
       <div className="row">
         {promos.map((promo) => (
-          <div key={promo.id} className="col-md-3 col-sm-6 p-3">
-            <div className="card ">
-              <img src={promo.imageUrl} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">{promo.title}</h5>
-                <p className="card-text">{promo.description}</p>
-                <a href="#" className="btn btn-outline-success">
-                  View Detail<i className="bi bi-arrow-right ms-2"></i>
-                </a>
-              </div>
-            </div>
-          </div>
+          <CardPromo promo={promo} key={promo.id} />
         ))}
       </div>
     </div>
