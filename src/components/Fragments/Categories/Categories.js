371cc14ed67016e0f24f0fc5bc2bcb4299011682
danimisme <msubhanr53@gmail.com>
2024-04-06 13:39:19 +0700

    use alice-carousels

--- src/components/Fragments/Categories/Categories.js
+++ src/components/Fragments/Categories/Categories.js
@@ -1,11 +1,26 @@
 "use client";
 import useGetData from "@/services/useGetData";
 import { useEffect, useState } from "react";
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 export default function Categories() {
   const [Categories, setCategories] = useState([]);
   const { getData } = useGetData();
 
+  const responsive = {
+    375: {
+      items: 1,
+    },
+    568: {
+      items: 2,
+    },
+    800: {
+      items: 4,
+      itemsFit: "contain",
+    },
+  };
   useEffect(() => {
     getData("categories").then((res) => setCategories(res.data.data));
   }, []);
@@ -15,12 +30,25 @@ export default function Categories() {
       <h2>
         <i className="bi bi-geo-alt-fill"></i> Temukan Yang Kamu Sukai
       </h2>
-      {Categories.map((category) => (
-        <div key={category.id}>
-          <p>{category.name}</p>
-          <img src={category.imageUrl} alt={category.name} />
-        </div>
-      ))}
+      <AliceCarousel
+        mouseTracking
+        infinite
+        disableDotsControls
+        responsive={responsive}
+      >
+        {Categories.map((category) => (
+          <div key={category.id}>
+            <p className="position-absolute p-3 fw-bold text-white bg-dark rounded bg-opacity-75">
+              {category.name}
+            </p>
+            <img
+              src={category.imageUrl}
+              alt={category.name}
+              className="img-fluid w-100 h-100"
+            />
+          </div>
+        ))}
+      </AliceCarousel>
     </div>
   );
 }
