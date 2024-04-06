22050551e84a6d6c638d4aa507f9e709724846d7
danimisme <msubhanr53@gmail.com>
2024-04-06 13:23:33 +0700

    get categories data

--- src/components/Fragments/Category/Category.js
+++ src/components/Fragments/Category/Category.js
@@ -1,9 +1,26 @@
+"use client";
+import useGetData from "@/services/useGetData";
+import { useEffect, useState } from "react";
+
 export default function Category() {
+  const [Categories, setCategories] = useState([]);
+  const { getData } = useGetData();
+
+  useEffect(() => {
+    getData("categories").then((res) => setCategories(res.data.data));
+  }, []);
+
   return (
     <div>
       <h2>
-        <i class="bi bi-geo-alt-fill"></i>Temukan Yang Kamu Sukai
+        <i className="bi bi-geo-alt-fill"></i> Temukan Yang Kamu Sukai
       </h2>
+      {Categories.map((category) => (
+        <div key={category.id}>
+          <p>{category.name}</p>
+          <img src={category.imageUrl} alt={category.name} />
+        </div>
+      ))}
     </div>
   );
 }
