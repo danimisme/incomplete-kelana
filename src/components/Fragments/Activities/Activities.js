a2128282d25db59fc9d4b29c9b780c73c6cb6ecb
danimisme <msubhanr53@gmail.com>
2024-04-08 21:48:46 +0700

    refactoring code , use card activity component

--- src/components/Fragments/Activities/Activities.js
+++ src/components/Fragments/Activities/Activities.js
@@ -3,6 +3,7 @@ import useGetData from "@/services/useGetData";
 import { useEffect, useState } from "react";
 import styles from "./Activities.module.css";
 import { Montserrat } from "next/font/google";
+import CardActivity from "../CardActivity/CardActivity";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -85,40 +86,7 @@ export default function Activities() {
         )}
         {activities.map((activity, index) => {
           if (index < 6) {
-            return (
-              <div key={activity.id} className=" col-md-6 col-lg-4  p-3 ">
-                <div className="card">
-                  <img
-                    src={activity.imageUrls}
-                    className={` ${styles.img} card-img-top`}
-                    alt="..."
-                  />
-                  <p className="position-absolute top-0 end-0 bg-white m-1 px-2 py-1 rounded-pill ">
-                    <i className="bi bi-star-fill text-warning"></i>{" "}
-                    {activity.rating}
-                  </p>
-                  <div className="card-body d-flex justify-content-between align-items-center">
-                    <div>
-                      <h6 className={` ${styles.title} card-title`}>
-                        {activity.title}
-                      </h6>
-                      <p className={` ${styles.text_location}`}>
-                        <i className="bi bi-geo-alt-fill text-success"></i>
-                        {` ${activity.city}, ${activity.province}`}
-                      </p>
-                    </div>
-                    <div>
-                      <p>
-                        {activity.price.toLocaleString("id-ID", {
-                          style: "currency",
-                          currency: "IDR",
-                        })}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
+            return <CardActivity key={activity.id} activity={activity} />;
           }
         })}
       </div>
