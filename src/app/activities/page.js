1460dc9c2df6dc83590dcb50fcc296dc63581702
danimisme <msubhanr53@gmail.com>
2024-04-08 21:52:40 +0700

    add CardActivity

--- src/app/activities/page.js
+++ src/app/activities/page.js
@@ -1,6 +1,7 @@
 "use client";
 import useGetData from "@/services/useGetData";
 import { useEffect, useState } from "react";
+import CardActivity from "@/components/Fragments/CardActivity/CardActivity";
 
 export default function ActivityPage() {
   const { getData } = useGetData();
@@ -8,11 +9,15 @@ export default function ActivityPage() {
   useEffect(() => {
     getData("activities").then((res) => setActivities(res.data.data));
   }, []);
-  console.log(activities);
   return (
     <div className="mt-5 container-lg">
       <div className="py-5">
         <h1>Activity Page</h1>
+        <div className="row">
+          {activities.map((activity) => (
+            <CardActivity activity={activity} key={activity.id} />
+          ))}
+        </div>
       </div>
     </div>
   );
