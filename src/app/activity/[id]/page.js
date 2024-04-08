2830a5e4ee4dbf5468f5b905973ea30dbb1a75d5
danimisme <msubhanr53@gmail.com>
2024-04-08 22:11:39 +0700

    get activity data by ID

--- src/app/activity/[id]/page.js
+++ src/app/activity/[id]/page.js
@@ -1,6 +1,16 @@
+"use client";
+import useGetData from "@/services/useGetData";
+import { useEffect, useState } from "react";
+
 export default function DetailActivityPage({ params }) {
+  const { getData } = useGetData();
+  const [activity, setActivity] = useState({});
+  useEffect(() => {
+    getData(`activity/${params.id}`).then((res) => setActivity(res.data.data));
+  }, []);
+  console.log(activity);
   return (
-    <div>
+    <div className="mt-5 container-lg">
       <h1>Detail Activity Page</h1>
       <p>Activity ID: {params.id}</p>
     </div>
