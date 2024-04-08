794a2b7327d6f432e293496b8d6754739e38e0da
danimisme <msubhanr53@gmail.com>
2024-04-08 21:32:55 +0700

    get activities data

--- src/app/activity/page.js
+++ src/app/activity/page.js
@@ -1,4 +1,14 @@
+"use client";
+import useGetData from "@/services/useGetData";
+import { useEffect, useState } from "react";
+
 export default function ActivityPage() {
+  const { getData } = useGetData();
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    getData("activities").then((res) => setActivities(res.data.data));
+  }, []);
+  console.log(activities);
   return (
     <div className="mt-5 container-lg">
       <div className="py-5">
