703fd878d982c31fa8b88186aa85266a6fa9b591
danimisme <msubhanr53@gmail.com>
2024-04-08 21:30:40 +0700

    create activity page

---
+++ src/app/activity/page.js
@@ -0,0 +1,9 @@
+export default function ActivityPage() {
+  return (
+    <div className="mt-5 container-lg">
+      <div className="py-5">
+        <h1>Activity Page</h1>
+      </div>
+    </div>
+  );
+}
