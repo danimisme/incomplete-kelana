f8fe7026e2bdc593ea27a3838a4e2803c10b8ef3
danimisme <msubhanr53@gmail.com>
2024-04-12 22:26:10 +0700

    change some style and add default value

--- src/components/Fragments/FormUser/FormUser.js
+++ src/components/Fragments/FormUser/FormUser.js
@@ -8,20 +8,44 @@ export default function EditUserForm({ user }) {
   const dispatch = useDispatch();
   const isFormUserOpen = useSelector((state) => state.formUser.isFormUserOpen);
   return (
-    <div className={styles.form_container}>
+    <div
+      className={` ${styles.form_container} ${
+        isFormUserOpen ? styles.show : styles.hide
+      }`}
+    >
       <form className={styles.form}>
         <h2>Edit User</h2>
-        <i className={`${styles.close_btn} bi bi-x-circle fs-3`}></i>
-        <div className="mb-3">
-          <img src={user?.profilePictureUrl} alt="" />
+        <i
+          className={`${styles.close_btn} bi bi-x-circle fs-3`}
+          onClick={() => dispatch(toggleFormUser())}
+        ></i>
+        <div className="mb-3 d-flex align-items-center gap-3">
+          <img
+            src={user?.profilePictureUrl}
+            alt="Profile Picture"
+            className={styles.image}
+          />
+          <div>
+            <Input
+              type="file"
+              name="profilePictureUrl"
+              id="profilePictureUrl"
+            />
+            <button className="btn btn-success mt-3">Upload</button>
+          </div>
         </div>
         <div className=" mb-3">
           <Label htmlFor="name">Name</Label>
-          <Input type="text" name="name" id="name" placeholder="Your Name" />
+          <Input type="text" name="name" id="name" defaultValue={user?.name} />
         </div>
         <div className=" mb-3">
           <Label htmlFor="email">Email</Label>
-          <Input type="email" name="email" id="email" placeholder="Email" />
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            defaultValue={user?.email}
+          />
         </div>
         <div className=" mb-3">
           <Label htmlFor="phoneNumber">Phone Number</Label>
@@ -29,9 +53,10 @@ export default function EditUserForm({ user }) {
             type="text"
             name="phoneNumber"
             id="phoneNumber"
-            placeholder="Your Name"
+            defaultValue={user?.phoneNumber}
           />
         </div>
+        <button className="btn btn-success">Submit</button>
       </form>
     </div>
   );
