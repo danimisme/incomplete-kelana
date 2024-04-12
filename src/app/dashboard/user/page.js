c08b5205a747905805e794756fe31ec4f767297a
danimisme <msubhanr53@gmail.com>
2024-04-12 19:21:21 +0700

    create user page

---
+++ src/app/dashboard/user/page.js
@@ -0,0 +1,11 @@
+export default function UserPage() {
+  return (
+    <div className="mt-5 container-lg ">
+      <div className="row py-3">
+        <div className="col-lg-8 col-10 mx-auto">
+          <h1>User Page</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Layout/Sidebar/Sidebar.js
+++ src/components/Layout/Sidebar/Sidebar.js
@@ -1,8 +1,13 @@
 "use client";
 import styles from "./Sidebar.module.css";
 import { useSelector, useDispatch } from "react-redux";
+import {
+  showSidebar,
+  hideSidebar,
+  toggleSidebar,
+} from "@/redux/slices/SidebarSlice";
 export default function Sidebar() {
-  const sidebarShow = useSelector((state) => state.sidebarShow);
+  const sidebarShow = useSelector((state) => state.sidebar.sidebarShow);
   const dispacth = useDispatch();
   return (
     <div
@@ -10,7 +15,21 @@ export default function Sidebar() {
         sidebarShow ? styles.show : styles.hide
       }`}
     >
-      <h1>Sidebar</h1>
+      <div className={styles.button} onClick={() => dispacth(toggleSidebar())}>
+        {sidebarShow ? (
+          <i className="bi bi-arrow-left-circle fs-1 "></i>
+        ) : (
+          <i className="bi bi-arrow-right-circle fs-1 "></i>
+        )}
+      </div>
+      <h2>Dashboard</h2>
+      <ul className="list-group list-group-flush mt-5">
+        <li className="list-group-item">User</li>
+        <li className="list-group-item">Banner</li>
+        <li className="list-group-item">Promo</li>
+        <li className="list-group-item">Categoty</li>
+        <li className="list-group-item">Activity</li>
+      </ul>
     </div>
   );
 }
