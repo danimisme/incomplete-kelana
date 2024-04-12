5f8576a42df21b1163051220a76d650b21058fe0
danimisme <msubhanr53@gmail.com>
2024-04-12 15:49:54 +0700

    change font style

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-import { Merriweather, Gothic_A1 } from "next/font/google";
+import { Merriweather, Gothic_A1, Gabarito } from "next/font/google";
 import styles from "./page.module.css";
 import Banner from "@/components/Fragments/Banner/Banner";
 import Promo from "@/components/Fragments/Promo/Promo";
 import Categories from "@/components/Fragments/Categories/Categories";
 import Activities from "@/components/Fragments/Activities/Activities";
+
+const gabarito = Gabarito({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
 
 const merriweather = Merriweather({
   subsets: ["latin"],
@@ -16,27 +21,13 @@ const gothic = Gothic_A1({
 });
 
 export default function Home() {
-  const handleReset = () => {
-    const select = document.getElementById("select_categories");
-    select.value = "Select";
-    getData("activities").then((res) => setActivities(res.data.data));
-  };
-
-  const handleFilter = async () => {
-    const select = document.getElementById("select_categories");
-    const value = select.value;
-    getData(`activities-by-category/${value}`).then((res) =>
-      setActivities(res.data.data)
-    );
-  };
-
   return (
     <main>
       <div
         className={` ${styles.jumbotron} d-flex align-items-center justify-content-center text-white`}
       >
         <h1
-          className={`${gothic.className} ${styles.text_shadow} text-center fs-1`}
+          className={`${gabarito.className} ${styles.text_shadow} text-center fs-1`}
         >
           Adventure to Explore <br /> Through the Beautiful World
         </h1>
