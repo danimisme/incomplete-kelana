866eb15ff9447ab63e052e04ea73788a9b2bc539
danimisme <msubhanr53@gmail.com>
2024-04-12 17:22:00 +0700

    change style of side bar

--- src/components/Layout/Sidebar/Sidebar.js
+++ src/components/Layout/Sidebar/Sidebar.js
@@ -1,7 +1,15 @@
+"use client";
 import styles from "./Sidebar.module.css";
+import { useSelector, useDispatch } from "react-redux";
 export default function Sidebar() {
+  const sidebarShow = useSelector((state) => state.sidebarShow);
+  const dispacth = useDispatch();
   return (
-    <div className={styles.sidebar}>
+    <div
+      className={` ${styles.sidebar} ${
+        sidebarShow ? styles.show : styles.hide
+      }`}
+    >
       <h1>Sidebar</h1>
     </div>
   );
