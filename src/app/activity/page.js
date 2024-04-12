2101f92e38b9aa96de8a89fdfe78d8b0ab6d37be
danimisme <msubhanr53@gmail.com>
2024-04-12 12:48:56 +0700

    add background image for header

--- src/app/activity/page.js
+++ src/app/activity/page.js
@@ -2,6 +2,7 @@
 import useGetData from "@/services/useGetData";
 import { useEffect, useState } from "react";
 import CardActivity from "@/components/Fragments/CardActivity/CardActivity";
+import styles from "./ActivityPage.module.css";
 
 export default function ActivityPage() {
   const { getData } = useGetData();
@@ -10,15 +11,18 @@ export default function ActivityPage() {
     getData("activities").then((res) => setActivities(res.data.data));
   }, []);
   return (
-    <div className="mt-5 container-lg">
-      <div className="py-5">
-        <h1>Activity Page</h1>
-        <div className="row">
-          {activities.map((activity) => (
-            <CardActivity activity={activity} key={activity.id} />
-          ))}
+    <>
+      <div className={`${styles.jumbotron} `}></div>
+      <div className="mt-5 container-lg">
+        <div className="py-5">
+          <h1>Activity Page</h1>
+          <div className="row">
+            {activities.map((activity) => (
+              <CardActivity activity={activity} key={activity.id} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
