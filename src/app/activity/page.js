b90fef5ab24025e011c26b3bfa78fa4d703eb12f
danimisme <msubhanr53@gmail.com>
2024-04-12 13:01:47 +0700

    add gabarito font

--- src/app/activity/page.js
+++ src/app/activity/page.js
@@ -3,6 +3,12 @@ import useGetData from "@/services/useGetData";
 import { useEffect, useState } from "react";
 import CardActivity from "@/components/Fragments/CardActivity/CardActivity";
 import styles from "./ActivityPage.module.css";
+import { Gabarito } from "next/font/google";
+
+const gabarito = Gabarito({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
 
 export default function ActivityPage() {
   const { getData } = useGetData();
@@ -15,7 +21,9 @@ export default function ActivityPage() {
       <div
         className={`${styles.jumbotron} d-flex align-items-center justify-content-center text-white `}
       >
-        <h1 className={`${styles.text_shadow} text-center fs-1`}>
+        <h1
+          className={`${styles.text_shadow} ${gabarito.className} text-center fs-1 fw-bold`}
+        >
           Challenge, Adventure, Discovery. <br /> Explore the World with Us!
         </h1>
       </div>
