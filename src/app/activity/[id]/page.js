4a459d99cd0dd293bd1a775920a55c861bca63eb
danimisme <msubhanr53@gmail.com>
2024-04-12 15:14:32 +0700

    get all detail activity data

--- src/app/activity/[id]/page.js
+++ src/app/activity/[id]/page.js
@@ -1,19 +1,85 @@
 "use client";
 import useGetData from "@/services/useGetData";
 import { useEffect, useState } from "react";
+import styles from "./DetailActivityPage.module.css";
 
 export default function DetailActivityPage({ params }) {
   const { getData } = useGetData();
   const [activity, setActivity] = useState({});
+  const [mapHtml, setMapHtml] = useState("");
   useEffect(() => {
     getData(`activity/${params.id}`).then((res) => setActivity(res.data.data));
   }, []);
   console.log(activity);
+
+  useEffect(() => {
+    getData(`activity/${params.id}`).then((res) => {
+      setMapHtml(res.data.data.location_maps);
+    });
+  }, [activity]);
+
   return (
     <>
-      <div className="mt-5 container-lg">
-        <h1>Detail Activity Page</h1>
-        <p>Activity ID: {params.id}</p>
+      <div className="container-lg mt-5 py-3"></div>
+      <div className="container-lg">
+        <h1 className="text-center">{activity.title}</h1>
+        <div className="row">
+          {activity?.imageUrls?.map((image) => (
+            <div key={image} className=" col-lg-6 col-10 mx-auto py-3">
+              <img
+                src={image}
+                alt={activity.title}
+                className={`${styles.image} img-fluid`}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="container-lg row border border-1 border-dark">
+          <div className="col-lg-6 col-10 mx-auto mt-3">
+            <p>{activity.description}</p>
+            <p>
+              <span className="fw-bold"> Price : </span>
+              {activity?.price?.toLocaleString("id-ID", {
+                style: "currency",
+                currency: "IDR",
+              })}
+            </p>
+            <p>
+              <span className="fw-bold"> Price Discount : </span>
+              {activity?.price_discount?.toLocaleString("id-ID", {
+                style: "currency",
+                currency: "IDR",
+              })}
+            </p>
+            <p>
+              <span className="fw-bold">Facilities : </span>
+              {activity.facilities}
+            </p>
+            <p>
+              <span className="fw-bold"> Rating : </span>
+              <i className="bi bi-star-fill text-warning"></i> {activity.rating}
+              /5 (<i className="bi bi-person-fill"></i>
+              {activity.total_reviews} Reviews)
+            </p>
+            <p>
+              <span className="fw-bold"> Address : </span> {activity.address}
+            </p>
+            <p>
+              <span className="fw-bold"> City : </span> {activity.city}
+            </p>
+            <p>
+              <span className="fw-bold"> Province : </span> {activity.province}
+            </p>
+          </div>
+          <div className="col-lg-6 col-10 mx-auto mt-3">
+            <div className={styles.map_container}>
+              <div
+                dangerouslySetInnerHTML={{ __html: mapHtml }}
+                id="map-container"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
