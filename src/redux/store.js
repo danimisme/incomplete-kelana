0b57e1f997cd1eb2a291266e12640d3de60c6eb4
danimisme <msubhanr53@gmail.com>
2024-04-12 21:56:11 +0700

    add formUserSlice.reducer to store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { sidebarSlice } from "./slices/SidebarSlice";
+import { formUserSlice } from "./slices/FormUserSlice";
 
 export const store = configureStore({
   reducer: {
     sidebar: sidebarSlice.reducer,
+    formUser: formUserSlice.reducer,
   },
 });
 
