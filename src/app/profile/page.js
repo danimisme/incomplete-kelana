4b52d2ea743e50f9e884449634bed7e5564731ab
danimisme <msubhanr53@gmail.com>
2024-04-12 23:32:51 +0700

    create user Profile Page

---
+++ src/app/profile/page.js
@@ -0,0 +1,10 @@
+export default function ProfilePage() {
+  return (
+    <div className="mt-5 container-lg">
+      <div className="row py-3">
+        <h2>Profile Page</h2>
+        <div className="col-lg-6"> </div>
+      </div>
+    </div>
+  );
+}
