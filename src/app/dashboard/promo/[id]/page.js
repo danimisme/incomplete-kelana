c997abaf1ee52f5d03bad0f972ced07cb1407dca
danimisme <msubhanr53@gmail.com>
2024-04-13 16:34:34 +0700

    get data promo by id

--- src/app/dashboard/promo/[id]/page.js
+++ src/app/dashboard/promo/[id]/page.js
@@ -1,4 +1,13 @@
+"use client";
+import useGetData from "@/services/useGetData";
+import { useEffect, useState } from "react";
 export default function DetailPromoPage({ params }) {
+  const { getData } = useGetData();
+  const [promo, setPromo] = useState({});
+  useEffect(() => {
+    getData(`promo/${params.id}`).then((res) => setPromo(res?.data?.data));
+  }, []);
+  console.log(promo);
   return (
     <div className="mt-5 container-lg">
       <h1>Detail Promo Page</h1>
