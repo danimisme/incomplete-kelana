8140f0b8f395dc2ff9b32b8ac29acf67aa0c354f
danimisme <msubhanr53@gmail.com>
2024-04-13 17:00:44 +0700

    create form for edit promo

--- src/app/dashboard/promo/[id]/page.js
+++ src/app/dashboard/promo/[id]/page.js
@@ -1,5 +1,8 @@
 "use client";
+import Input from "@/components/Elements/input/Input";
+import Label from "@/components/Elements/input/Label";
 import useGetData from "@/services/useGetData";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 export default function DetailPromoPage({ params }) {
   const { getData } = useGetData();
@@ -10,7 +13,93 @@ export default function DetailPromoPage({ params }) {
   console.log(promo);
   return (
     <div className="mt-5 container-lg">
-      <h1>Detail Promo Page</h1>
+      <div className="row py-5">
+        <h1 className="text-center mb-3">Edit Promo</h1>
+        <div className="col-lg-6 col-10 mx-auto">
+          <img src={promo.imageUrl} alt={promo.title} className="img-fluid" />
+          <div className="my-3">
+            <Label className="form-label" htmlFor="imageUrl">
+              Image File
+            </Label>
+            <Input type="file" className="form-control" id="imageUrl" />
+            <button className="btn btn-primary mt-2" type="button">
+              Upload Image
+            </button>
+          </div>
+        </div>
+        <div className="col-lg-6 col-10 mx-auto">
+          <form action="">
+            <div className="mb-3">
+              <Label htmlFor="title" className="form-label">
+                Title
+              </Label>
+              <Input
+                className="form-control"
+                id="title"
+                defaultValue={promo.title}
+              />
+            </div>
+            <div className="mb-3">
+              <Label htmlFor="description" className="form-label">
+                Description
+              </Label>
+              <textarea
+                className="form-control"
+                id="description"
+                rows="3"
+                defaultValue={promo.description}
+              ></textarea>
+            </div>
+            <div className="mb-3">
+              <Label htmlFor="promo_code" className="form-label">
+                Promo Code
+              </Label>
+              <Input
+                className="form-control"
+                id="promo_code"
+                defaultValue={promo.promo_code}
+              />
+            </div>
+            <div className="mb-3">
+              <Label htmlFor="minimum_claim_price" className="form-label">
+                Minimum Claim Price
+              </Label>
+              <Input
+                type="number"
+                className="form-control"
+                id="minimum_claim_price"
+                defaultValue={promo.minimum_claim_price}
+              />
+            </div>
+            <div className="mb-3">
+              <Label htmlFor="terms_condition" className="form-label">
+                Terms & Conditions
+              </Label>
+              <Input
+                id="terms_condition"
+                defaultValue={promo.terms_condition}
+              />
+            </div>
+            <div className="mb-3">
+              <Label htmlFor="promo_discount_price" className="form-label">
+                Promo Discount Price
+              </Label>
+              <Input
+                type="number"
+                className="form-control"
+                id="promo_discount_price"
+                defaultValue={promo.promo_discount_price}
+              />
+            </div>
+            <button type="submit" className="btn btn-success">
+              Edit Promo
+            </button>
+            <Link href="/dashboard/promo">
+              <button className="btn btn-secondary mx-2">Back</button>
+            </Link>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
