d542b04ff918af66247d9fc4e82ab070476158a7
danimisme <msubhanr53@gmail.com>
2024-04-13 15:42:14 +0700

    create table data

--- src/app/dashboard/promo/page.js
+++ src/app/dashboard/promo/page.js
@@ -1,7 +1,53 @@
+"use client";
+import useGetData from "@/services/useGetData";
+import { useEffect, useState } from "react";
+
 export default function PromoPage() {
+  const { getData } = useGetData();
+  const [promos, setPromos] = useState([]);
+  useEffect(() => {
+    getData("promos").then((res) => setPromos(res.data.data));
+  }, []);
   return (
     <div className="mt-5 container-lg">
-      <h1>Promo Page</h1>
+      <div className="row py-5">
+        <div className="col-lg-10 col-10 mx-auto ">
+          <h1>Data Promo</h1>
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col" className="d-none d-md-table-cell">
+                  Description
+                </th>
+                <th scope="col" colSpan={2} className="text-center">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {promos.map((promo, index) => (
+                <tr key={promo.id}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{promo.title}</td>
+                  <td className="d-none d-md-table-cell">
+                    {promo.description.length > 50
+                      ? promo.description.slice(0, 50) + " . . ."
+                      : promo.description}
+                  </td>
+                  <td>
+                    <button className="btn btn-outline-success">Edit</button>
+                  </td>
+                  <td>
+                    <button className="btn btn-outline-danger">Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
