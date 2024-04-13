8fd61735370e6080a78a942b992fc722fb3dd784
danimisme <msubhanr53@gmail.com>
2024-04-13 20:53:33 +0700

    add userLoggedSlice to Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { sidebarSlice } from "./slices/SidebarSlice";
 import { formUserSlice } from "./slices/FormUserSlice";
 import { formBannerSlice } from "./slices/FormBannerSlice";
+import { userLoggedSlice } from "./slices/UserLoggedSlice";
 
 export const store = configureStore({
   reducer: {
     sidebar: sidebarSlice.reducer,
     formUser: formUserSlice.reducer,
     formBanner: formBannerSlice.reducer,
+    userLogged: userLoggedSlice.reducer,
   },
 });
 
