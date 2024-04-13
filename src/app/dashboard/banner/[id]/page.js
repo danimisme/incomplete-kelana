4fff7cae42af411efa302739eb69388ad8b6c24d
danimisme <msubhanr53@gmail.com>
2024-04-13 10:13:53 +0700

    get banner data by id

--- src/app/dashboard/banner/[id]/page.js
+++ src/app/dashboard/banner/[id]/page.js
@@ -1,4 +1,13 @@
+"use client";
+import useGetData from "@/services/useGetData";
+import { useEffect, useState } from "react";
+
 export default function DetailBannerPage({ params }) {
+  const { getData } = useGetData();
+  const [banner, setBanner] = useState({});
+  useEffect(() => {
+    getData(`banners/${params.id}`).then((res) => setBanner(res?.data?.data));
+  }, []);
   return (
     <div className="mt-5 container-lg">
       <h1>Detail Banner</h1>
