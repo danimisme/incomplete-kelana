9a5c8f644c70340bd210f9757d704417d652dfa6
danimisme <msubhanr53@gmail.com>
2024-04-14 15:46:02 +0700

    get category data using useGetData

--- src/app/dashboard/category/page.js
+++ src/app/dashboard/category/page.js
@@ -1,4 +1,13 @@
+"use client";
+import useGetData from "@/services/useGetData";
+import { useEffect, useState } from "react";
 export default function CategoryPage() {
+  const { getData } = useGetData();
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    getData("categories").then((res) => setCategories(res.data.data));
+  }, []);
+  console.log(categories);
   return (
     <div className="mt-5 container-lg">
       <div className="row py-5">
