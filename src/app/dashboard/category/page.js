cc43806b62f5a50129b77f0bdd41312473161761
danimisme <msubhanr53@gmail.com>
2024-04-14 16:05:07 +0700

    create table for data category

--- src/app/dashboard/category/page.js
+++ src/app/dashboard/category/page.js
@@ -11,7 +11,43 @@ export default function CategoryPage() {
   return (
     <div className="mt-5 container-lg">
       <div className="row py-5">
-        <h1>Category Page</h1>
+        <div className="col-lg-10 col-10 mx-auto">
+          <h1>Category Data</h1>
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Picture</th>
+                <th scope="col" className="text-center">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {categories.map((category, index) => (
+                <tr key={category.id}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{category.name}</td>
+                  <td>
+                    <img
+                      src={category.imageUrl}
+                      alt={category.name}
+                      width={100}
+                      height={100}
+                    />
+                  </td>
+                  <td className="text-center">
+                    <button className="btn btn-outline-success me-2 mb-2 mb-md-0">
+                      Edit
+                    </button>
+                    <button className="btn btn-outline-danger">Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
