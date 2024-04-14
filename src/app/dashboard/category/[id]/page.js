7828a98a6264b546a31d0aad34f531afcac7f039
danimisme <msubhanr53@gmail.com>
2024-04-14 16:22:22 +0700

    create detail category page

---
+++ src/app/dashboard/category/[id]/page.js
@@ -0,0 +1,9 @@
+export default function DetailCategoryPage() {
+  return (
+    <div className="container-lg mt-5">
+      <div className="row py-5">
+        <h1>Detail Category</h1>
+      </div>
+    </div>
+  );
+}
