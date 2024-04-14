9d86dc81e7fa7178d25ebaaf4e536458a9d3ef8a
danimisme <msubhanr53@gmail.com>
2024-04-14 15:39:27 +0700

    create category page

---
+++ src/app/dashboard/category/page.js
@@ -0,0 +1,9 @@
+export default function CategoryPage() {
+  return (
+    <div className="mt-5 container-lg">
+      <div className="row py-5">
+        <h1>Category Page</h1>
+      </div>
+    </div>
+  );
+}
