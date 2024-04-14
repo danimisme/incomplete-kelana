6c9cb9deffdd638851578987de0e4abb2d489349
danimisme <msubhanr53@gmail.com>
2024-04-14 16:57:04 +0700

    get data categoty by id

--- src/app/dashboard/category/[id]/page.js
+++ src/app/dashboard/category/[id]/page.js
@@ -1,4 +1,16 @@
-export default function DetailCategoryPage() {
+"use client";
+import useGetData from "@/services/useGetData";
+import { useEffect, useState } from "react";
+
+export default function DetailCategoryPage({ params }) {
+  const { getData } = useGetData();
+  const [category, setCategory] = useState();
+  useEffect(() => {
+    getData(`category/${params.id}`).then((res) =>
+      setCategory(res?.data?.data)
+    );
+  }, []);
+  console.log(category);
   return (
     <div className="container-lg mt-5">
       <div className="row py-5">
