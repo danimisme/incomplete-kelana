a5ef1ad33503d9c2c27ee58083748d8cbe6ba2a5
danimisme <msubhanr53@gmail.com>
2024-04-15 21:55:11 +0700

    create activity table

--- src/app/dashboard/activity/page.js
+++ src/app/dashboard/activity/page.js
@@ -8,11 +8,45 @@ export default function ActivityPage() {
   useEffect(() => {
     getData("activities").then((res) => setActivities(res.data.data));
   }, []);
-  console.log(activities);
   return (
     <div className="mt-5 container-lg">
       <div className="row py-5">
-        <h1>Activity Data</h1>
+        <div className="col-lg-10 col-10 mx-auto">
+          <h1>Activity Data</h1>
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col">Category</th>
+                <th scope="col" colSpan={2} className="text-center">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {activities.map((activity, index) => (
+                <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{activity.title}</td>
+                  <td>
+                    {activity.description.length > 50
+                      ? activity.description.slice(0, 50) + "..."
+                      : activity.description}
+                  </td>
+                  <td>{activity.category.name}</td>
+                  <td>
+                    <button className="btn btn-outline-success me-2 my-2">
+                      Edit
+                    </button>
+                    <button className="btn btn-outline-danger">Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
