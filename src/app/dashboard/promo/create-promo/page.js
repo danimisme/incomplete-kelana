564dbde3f259a696e06f036a6e84e993f45045fd
danimisme <msubhanr53@gmail.com>
2024-04-15 17:39:20 +0700

    create create-promo page

---
+++ src/app/dashboard/promo/create-promo/page.js
@@ -0,0 +1,9 @@
+export default function CreatePromoPage() {
+  return (
+    <div className="mt-5 container-lg">
+      <div className="row py-5">
+        <h1>Create Promo</h1>
+      </div>
+    </div>
+  );
+}
