a66c3d274dc61a90724fd096c8ebe43e0e64ff5f
danimisme <msubhanr53@gmail.com>
2024-04-15 22:14:58 +0700

    create DetailActivityPage

---
+++ src/app/dashboard/activity/[id]/page.js
@@ -0,0 +1,9 @@
+export default function DetailActivityPage() {
+  return (
+    <div className="container-lg mt-5">
+      <div className="row py-5">
+        <h1>Detail Activity</h1>
+      </div>
+    </div>
+  );
+}
