9947781682c76e92b51d844ae7812533b18b1e3d
danimisme <msubhanr53@gmail.com>
2024-04-18 22:54:43 +0700

    create handle Create and add form activity

--- src/app/dashboard/activity/create-activity/page.js
+++ src/app/dashboard/activity/create-activity/page.js
@@ -1,8 +1,26 @@
+"use client";
+import FormActivity from "@/components/Fragments/FormActivity/FormActivity";
+import useCreate from "@/services/useCreate";
+import { useRouter } from "next/navigation";
+
 export default function CreateActivityPage() {
+  const { create } = useCreate();
+  const router = useRouter();
+
+  const handleCreate = async (data) => {
+    try {
+      const res = await create("create-activity", data);
+      if (res.status === 200) {
+        router.push("/dashboard/activity");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="container-lg mt-5">
       <div className="row py-5">
-        <h1>Create Activity</h1>
+        <FormActivity onSubmit={handleCreate} />
       </div>
     </div>
   );
