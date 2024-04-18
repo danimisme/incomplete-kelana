e8fb722aa0d762df870990186df236a74c336f49
danimisme <msubhanr53@gmail.com>
2024-04-18 22:42:00 +0700

    create create-activity Page

---
+++ src/app/dashboard/activity/create-activity/page.js
@@ -0,0 +1,9 @@
+export default function CreateActivityPage() {
+  return (
+    <div className="container-lg mt-5">
+      <div className="row py-5">
+        <h1>Create Activity</h1>
+      </div>
+    </div>
+  );
+}
